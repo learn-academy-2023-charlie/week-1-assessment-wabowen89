97655df2e836bb3585ce8a7306f81c60161ea615
wabowen89 <wabowen89@gmail.com>
2023-04-21 17:11:03 -0700

    completion of coding-concepts

--- coding-concepts.js
+++ coding-concepts.js
@@ -32,23 +32,23 @@ console.log(greeting[4])
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: Ruby; By passing the variable 'index' in to the [] of languages, we are logging the value stored at the value provided by the variable 'index' within the languages array.
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: weekendDays.toUpperCase() is not a function; The .toUpperCase() is a method that operates on a string datatype. 'weekendDays' is an array datatype. Therefore, we are presented with the error message "not a function".
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Syntax Error
+// b) Verify and explain: "number"; 'typeof' is an operator the determines the datatype of given variable. '.length' returns a numerical value of the length of given array. When logging 'typeof dataTypes.length', the '.length' returns a numerical value, and the 'typeof' operator determines that the datatype returned by 'length' is a number datatype.
